af3a325411bf89224e152e467a5e5e17adb93547
Alex Seriy <alex@pagewire.com>
2016-03-14 09:45:00 -0400

    Initial check-in.

---
+++ Logical/OpenWeatherMap.scm/config.scm
@@ -0,0 +1,3 @@
+<?xml version="1.0" encoding="UTF-8"?><editableResource createdTimestamp="1456745314889" createdUser="rational" id="-fbdd4ef:1532ae26c6c:-7aca" type="service_component_resource" updatedTimestamp="1456745314889" updatedUser="rational" version="1.1">
+   <loggingConfiguration debug="false" writeStubEventsToDB="false"/>
+</editableResource>
